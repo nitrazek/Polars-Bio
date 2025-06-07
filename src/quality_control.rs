ef8b7d49a89cd543250387a28781dee0568edc3f
nitrazek <99548252+nitrazek@users.noreply.github.com>
2025-06-07 13:30:56 +0200

    No errors version

--- src/quality_control.rs
+++ src/quality_control.rs
@@ -1,19 +1,19 @@
-use arrow_array::{ArrayRef, ListArray, StructArray, UInt32Array, StringArray};
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use arrow::datatypes::UInt32Type;
+use arrow_array::{ArrayRef, ListArray, StringArray, StructArray, UInt32Array};
 use arrow_schema::{DataType, Field};
 use datafusion::arrow::ffi_stream::ArrowArrayStreamReader;
 use datafusion::arrow::pyarrow::PyArrowType;
-use datafusion::physical_plan::Accumulator;
-use datafusion::error::Result;
-use datafusion::scalar::ScalarValue;
-use exon::ExonSession;
-use polars::prelude::DataType;
-use tokio::runtime::Runtime;
-use pyo3::prelude::*;
 use datafusion::dataframe::DataFrame;
+use datafusion::error::Result;
+use datafusion::physical_plan::Accumulator;
+use datafusion::scalar::ScalarValue;
 use datafusion_python::dataframe::PyDataFrame;
-use arrow::datatypes::UInt32Type;
-use std::collections::HashMap;
-use std::sync::Arc;
+use exon::ExonSession;
+use pyo3::prelude::*;
+use tokio::runtime::Runtime;
 
 use crate::context::PyBioSessionContext;
 use crate::register_frame;
@@ -26,97 +26,121 @@ const T: usize = 3;
 const N: usize = 4;
 
 fn get_list_array(v: Vec<Option<u32>>) -> Arc<ListArray> {
-    Arc::new(ListArray::from_iter_primitive::<UInt32Type, _, _>(vec![Some(v.clone())]))
+    Arc::new(ListArray::from_iter_primitive::<UInt32Type, _, _>(vec![
+        Some(v.clone()),
+    ]))
 }
 
 fn get_struct_array(m: HashMap<&str, Vec<Option<u32>>>) -> Arc<StructArray> {
-    Arc::new(StructArray::from(m.into_iter().map(|(k, v)| {
-        (
-            Arc::new(Field::new(k, DataType::List(Arc::new(Field::new("item", DataType::UInt32, false))), false)),
-            get_list_array(v.clone()) as ArrayRef
-        )
-    }).collect::<Vec<(Arc<Field>, ArrayRef)>>()))
+    Arc::new(StructArray::from(
+        m.into_iter()
+            .map(|(k, v)| {
+                (
+                    Arc::new(Field::new(
+                        k,
+                        DataType::List(Arc::new(Field::new("item", DataType::UInt32, false))),
+                        false,
+                    )),
+                    get_list_array(v.clone()) as ArrayRef,
+                )
+            })
+            .collect::<Vec<(Arc<Field>, ArrayRef)>>(),
+    ))
 }
 
 #[derive(Debug)]
 struct BaseSequenceContent {
-    base_count: HashMap<u64, [u64; 5]>
+    base_count: HashMap<u64, [u64; 5]>,
 }
 
 impl BaseSequenceContent {
     pub fn new() -> Self {
         Self {
-            base_count: HashMap::new()
+            base_count: HashMap::new(),
         }
     }
 
     fn update_state(&mut self, s: &String) -> () {
         for (pos, base) in s.chars().enumerate() {
-            let pos_count = self.base_count.entry(pos as u64).or_insert_with(|| [0_u64; 5]);
+            let pos_count = self
+                .base_count
+                .entry(pos as u64)
+                .or_insert_with(|| [0_u64; 5]);
             let i = match base {
                 'A' | 'a' => 0,
                 'C' | 'c' => 1,
                 'G' | 'g' => 2,
                 'T' | 't' => 3,
                 'N' | 'n' => 4,
-                _ => panic!("Invalid base")
+                _ => panic!("Invalid base"),
             };
             pos_count[i] += 1;
         }
     }
 }
 
-impl Accumulator for BaseSequenceContent {
-    fn state(&mut self) -> Result<Vec<ScalarValue>> {
-        let mut structs = Vec::new();
-        let struct_datatype = DataType::Struct(vec![
-            Field::new("key", DataType::UInt64, false),
-            Field::new("values", DataType::List(Arc::new(Field::new("item", DataType::UInt32, false))))
-        ]);
-        for (i, array) in &self.base_count {
-            let values: Vec<ScalarValue> = array.iter().map(|&v| ScalarValue::from(v)).collect();
-            serialized_state.push(ScalarValue::List(ScalarValue::new_list(&values, &DataType::UInt64, false)));
-        }
-        Ok(serialized_state)
-    }
+// impl Accumulator for BaseSequenceContent {
+//     fn state(&mut self) -> Result<Vec<ScalarValue>> {
+//         let mut structs = Vec::new();
+//         let struct_datatype = DataType::Struct(vec![
+//             Field::new("key", DataType::UInt64, false),
+//             Field::new(
+//                 "values",
+//                 DataType::List(Arc::new(Field::new("item", DataType::UInt32, false))),
+//             ),
+//         ]);
+//         for (i, array) in &self.base_count {
+//             let values: Vec<ScalarValue> = array.iter().map(|&v| ScalarValue::from(v)).collect();
+//             serialized_state.push(ScalarValue::List(ScalarValue::new_list(
+//                 &values,
+//                 &DataType::UInt64,
+//                 false,
+//             )));
+//         }
+//         Ok(serialized_state)
+//     }
 
-    fn update_batch(&mut self, values: &[ArrayRef]) -> Result<()> {
-        let array = &values[0];
-        (0..array.len()).try_for_each(|i| {
-            let v = ScalarValue::try_from_array(array, i)?;
-            if let ScalarValue::Utf8(Some(value)) = v {
-                self.update_state(&value);
-            }
-            Ok(())
-        })
-    }
+//     fn update_batch(&mut self, values: &[ArrayRef]) -> Result<()> {
+//         let array = &values[0];
+//         (0..array.len()).try_for_each(|i| {
+//             let v = ScalarValue::try_from_array(array, i)?;
+//             if let ScalarValue::Utf8(Some(value)) = v {
+//                 self.update_state(&value);
+//             }
+//             Ok(())
+//         })
+//     }
 
-    fn merge_batch(&mut self, states: &[ArrayRef]) -> Result<()> {
-        
-    }
+//     fn merge_batch(&mut self, states: &[ArrayRef]) -> Result<()> {
+//         todo!()
+//     }
 
-    // fn evaluate(&mut self) -> Result<ScalarValue> {
-    //     let mut map: HashMap<&str, Vec<Option<u32>>> = HashMap::new();
-    //     map.insert("A_count", self.base_count[0].clone());
-    //     map.insert("C_count", self.base_count[1].clone());
-    //     map.insert("T_count", self.base_count[2].clone());
-    //     map.insert("G_count", self.base_count[3].clone());
-    //     map.insert("N_count", self.base_count[4].clone());
-    //     Ok(ScalarValue::Struct(get_struct_array(map))) 
-    // }
-}
+//     fn evaluate(&mut self) -> Result<ScalarValue> {
+//         todo!()
+//     }
 
+//     fn size(&self) -> usize {
+//         todo!()
+//     }
 
-pub(crate) async fn do_base_sequence_content(
-    ctx: &ExonSession,
-    table_name: String
-)-> DataFrame {
+//    fn evaluate(&mut self) -> Result<ScalarValue> {
+//        let mut map: HashMap<&str, Vec<Option<u32>>> = HashMap::new();
+//        map.insert("A_count", self.base_count[0].clone());
+//        map.insert("C_count", self.base_count[1].clone());
+//        map.insert("T_count", self.base_count[2].clone());
+//        map.insert("G_count", self.base_count[3].clone());
+//        map.insert("N_count", self.base_count[4].clone());
+//        Ok(ScalarValue::Struct(get_struct_array(map)))
+//    }
+// }
+
+pub(crate) async fn do_base_sequence_content(ctx: &ExonSession, table_name: String) -> DataFrame {
     ctx.sql("WITH my_map AS (SELECT MAP { 'key1': 'value1', 'key2': 'value2' } AS map) SELECT map_extract(map, 'key1') AS key1, map_extract(map, 'key2') AS key2 FROM my_map;").await.unwrap()
 }
 
 pub(crate) async fn do_test_base_sequence_content(
     ctx: &ExonSession,
-    table_name: String
+    table_name: String,
 ) -> DataFrame {
     let query = "
         WITH base_sequence_map AS (
@@ -137,5 +161,6 @@ pub(crate) async fn do_test_base_sequence_content(
         FROM base_sequence_map;
     ";
     let df: DataFrame = ctx.sql(&query).await.unwrap();
-    df.unnest_columns(&["a_count", "c_count", "t_count", "g_count", "n_count"]).unwrap()
+    df.unnest_columns(&["a_count", "c_count", "t_count", "g_count", "n_count"])
+        .unwrap()
 }
